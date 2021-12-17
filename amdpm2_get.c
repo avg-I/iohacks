d5c8d2a25cb04c6b2c257c1b2fde489ef617efad
Andriy Gapon <avg@icyb.net.ua>
2021-12-17 11:06:25 +0200

    tools for accessing PM and PM2 register blocks on AMD chipsets / SoCs
    They use indexed access via designated I/O ports.

---
+++ amdpm2_get.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <machine/cpufunc.h>
+
+static const u_int addr_port = 0xcd0;
+static const u_int data_port = 0xcd1;
+
+int main(int argc, char** argv)
+{
+	int fd;
+	u_char reg;
+	u_char val;
+
+	fd = open("/dev/io", O_RDONLY);
+	if (fd < 0) {
+		perror("open /dev/io");
+		return 1;
+	}
+
+	reg = strtoul(argv[1], NULL, 0);
+	outb(addr_port, reg);
+	val = inb(data_port);
+
+	printf("reg %#0.2X has value %#0.2X\n", reg, val);
+	close(fd);
+	return 0;
+}
+
---
+++ amdpm2_set.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <machine/cpufunc.h>
+
+static const u_int addr_port = 0xcd0;
+static const u_int data_port = 0xcd1;
+
+int main(int argc, char** argv)
+{
+	int fd;
+	u_char reg;
+	u_char val;
+
+	fd = open("/dev/io", O_RDONLY);
+	if (fd < 0) {
+		perror("open /dev/io");
+		return 1;
+	}
+
+	reg = strtoul(argv[1], NULL, 0);
+	val = strtoul(argv[2], NULL, 0);
+
+	outb(addr_port, reg);
+	outb(data_port, val);
+
+	printf("reg %#0.2X has value %#0.2X\n", reg, val);
+	close(fd);
+	return 0;
+}
+
